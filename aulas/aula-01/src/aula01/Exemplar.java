f4df47a7138334c8b06a1101f161916dea5b0531
alexandrossilva <alexandrossilva@gmail.com>
2025-03-21 19:45:32 -0300

    Códigos fonte apresentados em aulas dos dias 12 e 19/03/2025

--- aulas/aula-01/src/aula01/Exemplar.java
+++ aulas/aula-01/src/aula01/Exemplar.java
@@ -1,52 +1,59 @@
 package aula01;
 
+// Encapsulamento de dados típicos de exemplar
 public class Exemplar {
 
-    private int tombo;
-    private String titulo;
-    private String autores;
-    private boolean aptoEmprestimo;
-    private int emprestimos;
+	private int tombo;					// número de tombo
+	private String titulo;				// título
+	private String autores;				// nomes dos autores
+	private boolean aptoEmprestimo;		// exemplar apto (true) ou não (false) a ser emprestado
+	private int emprestimos;			// quantidade até então de empréstimos registrados do exemplar
 
-    public Exemplar(int tombo, String titulo, String autores, boolean aptoEmprestimo) {
-        this.tombo = tombo;
-        this.titulo = titulo;
-        this.autores = autores;
-        this.aptoEmprestimo = aptoEmprestimo;
-    }
+	// construtor
+	public Exemplar(int tombo, String titulo, String autores, 
+			boolean aptoEmprestimo) {
+		this.tombo = tombo;
+		this.titulo = titulo;
+		this.autores = autores;
+		this.aptoEmprestimo = aptoEmprestimo;
+	}
+	
+	// métodos getter
+	public int getTombo() {
+		return tombo;
+	}
 
-    public int getTombo() {
-        return tombo;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public String getAutores() {
+		return autores;
+	}
 
-    public String getAutores() {
-        return autores;
-    }
+	public boolean isAptoEmprestimo() {
+		return aptoEmprestimo;
+	}
 
-    public boolean isAptoEmprestimo() {
-        return aptoEmprestimo;
-    }
+	public int getEmprestimos() {
+		return emprestimos;
+	}
 
-    public int getEmprestimos() {
-        return emprestimos;
-    }
+	// habilitação de exemplar para que possa ser emprestado
+	public void habilitarEmprestimo() {
+		this.aptoEmprestimo = true;
+	}
 
-    public void habilitarEmprestimo() {
-        this.aptoEmprestimo = true;
-    }
+	// desabilitação de possibilidade de exemplar poder ser emprestado
+	public void desabilitarEmprestimo() {
+		this.aptoEmprestimo = false;
+	}
 
-    public void desabilitarEmprestimo() {
-        this.aptoEmprestimo = false;
-    }
-
-    public void adicionarEmprestimo() {
-        if (this.aptoEmprestimo) {
-            this.emprestimos += 1;
-        }
-    }
+	// atualização de quantidade de empréstimos do exemplar em 1 (uma) unidade
+	public void adicionarEmprestimo() {
+		if (this.aptoEmprestimo) {		// se exemplar apto a ser emprestado
+			this.emprestimos += 1;		// incremento de quantidade de empréstimos
+		}
+	}
 
 }
